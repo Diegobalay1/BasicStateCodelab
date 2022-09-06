f88892396c386ed0f9c87ed5ad6b256598fea442
balay <dlolhd@gmail.com>
2022-09-06 17:19:51 +0200

    Como recordar en la composicion.

--- app/src/main/java/com/diego/kotlin/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/diego/kotlin/basicstatecodelab/WaterCounter.kt
@@ -13,37 +13,18 @@ import com.diego.kotlin.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-
     Column(modifier = modifier.padding(16.dp)) {
-        // Changes to count are now tracked by Compose
-        //val count: MutableState<Int> = mutableStateOf(0)
-        //val count: MutableState<Int> = remember { mutableStateOf(0) }
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
             Text(
                 text = "You've had $count glasses.",
             )
         }
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(
-                onClick = { count++ },
-                enabled = count < 10) {
+        Button(
+            onClick = { count++ },
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10) {
                 Text("Add one")
-            }
-            Button(
-                onClick = { count = 0 },
-                Modifier.padding(start = 8.dp)
-            ) {
-                Text("Clear water count")
-            }
         }
     }
 }
