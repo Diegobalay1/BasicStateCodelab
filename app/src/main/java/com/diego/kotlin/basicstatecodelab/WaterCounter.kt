33d1e479e845f99ad2585757c0474820e6b51902
balay <dlolhd@gmail.com>
2022-09-06 08:28:54 +0200

    Memoria en una funcion de componibilidad.

--- app/src/main/java/com/diego/kotlin/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/diego/kotlin/basicstatecodelab/WaterCounter.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -12,8 +12,12 @@ import com.diego.kotlin.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+
     Column(modifier = modifier.padding(16.dp)) {
+        // Changes to count are now tracked by Compose
+        //val count: MutableState<Int> = mutableStateOf(0)
+        //val count: MutableState<Int> = remember { mutableStateOf(0) }
+        var count by remember { mutableStateOf(0) }
         Text(
             text = "You've had $count glasses.",
         )
