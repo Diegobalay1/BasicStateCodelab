6db3d91e5723339b5afd6ac4c17576f2c87baf12
balay <dlolhd@gmail.com>
2022-09-07 12:24:34 +0200

    Elevacion de estado.

--- app/src/main/java/com/diego/kotlin/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/diego/kotlin/basicstatecodelab/WaterCounter.kt
@@ -6,15 +6,38 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.diego.kotlin.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 @Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(
+                text = "You've had $count glasses.",
+            )
+        }
+        Button(
+            onClick = onIncrement,
+            Modifier.padding(top = 8.dp),
+            enabled = count < 10) {
+                Text("Add one")
+        }
+    }
+}
+/*@Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Text(
                 text = "You've had $count glasses.",
@@ -27,12 +50,12 @@ fun WaterCounter(modifier: Modifier = Modifier) {
                 Text("Add one")
         }
     }
-}
+}*/
 
 @Preview(showBackground = true)
 @Composable
-fun WaterCounterPreview() {
+fun WaterCounterScreenPreview() {
     BasicStateCodelabTheme {
-        WaterCounter()
+        StatefulCounter()
     }
 }
